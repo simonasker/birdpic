72eb965f430e2100a1f664beac84754c63d28773
Simon Andersson <simon@mullvad.net>
2016-04-29 23:01:22 +0200

    Add setup.py that works with py2exe

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+    windows=[{
+        'script': 'main.py'
+    }],
+    options={
+        'py2exe': {
+            'includes': [
+                'sip',
+                'PyQt4.QtGui',
+                'PyQt4.QtCore',
+                'matplotlib.backends.backend_tkagg',
+                'tkinter.filedialog',
+            ],
+            # 'excludes': ['tkinter'],
+            # 'bundle_files': 2,
+        }
+    },
+    # zipfile=None,
+)
