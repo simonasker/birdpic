1d0879d3694901f0b0e253fe0562dbdeb67b5d9a
Simon Andersson <simon@mullvad.net>
2016-05-08 16:10:26 +0200

    Update mac_setup.py

--- mac_setup.py
+++ mac_setup.py
@@ -4,12 +4,17 @@ from setuptools import setup
 APP = ['main.py']
 DATA_FILES = [('data', [
     'data/plumreg.csv',
+    'data/ioc.xml',
 ])]
 OPTIONS = {
     'argv_emulation': True,
 }
 
 setup(
+    name='DigBird',
+    version='0.1',
+    author='Simon Andersson',
+    author_email='simon.asker@gmail.com',
     app=APP,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
