78c6d4543a212addfa7ea0b5ab571692cbb8cf17
Simon Andersson <simon@mullvad.net>
2016-05-15 19:48:16 +0200

    Include DLLs and data files in windows setup

--- win_setup.py
+++ win_setup.py
@@ -1,14 +1,37 @@
 from setuptools import setup
-# from distutils.core import setup
 import py2exe
 
+DATA_FILES = [
+    ('', [
+        'dll/libiomp5md.dll',
+        'dll/mkl_core.dll',
+        'dll/mkl_def.dll',
+        'dll/mkl_intel_thread.dll',
+        # Only one of _def and _mc seem to be needed
+        # 'dll/mkl_mc.dll',
+    ]),
+    ('data', [
+        'data/plumreg.csv',
+        'data/ioc.xml',
+    ]),
+]
 
 setup(
     name='DigBird',
     version='0.1',
     author='Simon Andersson',
     author_email='simon.asker@gmail.com',
-    windows={
-        'script': 'main.py',
+    options={
+        'py2exe': {
+            'includes': [
+                'matplotlib.backends.backend_tkagg',
+                'scipy.linalg.cython_blas',
+                'scipy.linalg.cython_lapack',
+                'scipy.sparse.csgraph._validation',
+                'tkinter.filedialog',
+            ],
+        },
     },
+    data_files=DATA_FILES,
+    windows=['main.py'],
 )
